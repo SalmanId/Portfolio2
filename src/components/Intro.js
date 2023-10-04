c8ede40509d267e208d5ae73ead01b7d96e2c918
Id said Salman <salmanartist@03gmail.com>
2023-10-04 22:22:40 +0200

    V5

---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import Typewriter from "typewriter-effect";
+
+const Intro = () => {
+  return (
+    <h1
+      style={{ lineHeight: "1.4" }}
+      className="pr-6 py-8 pl-12 text-2xl sm:text-6xl w-11/12"
+    >
+      Hey, I am a software engineer & mobile developer,
+      
+    </h1>
+  );
+};
+
+export default Intro;
